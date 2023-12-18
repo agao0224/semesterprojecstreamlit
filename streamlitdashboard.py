6b0ed5a33d7712b6a9a8cf5bf5605b52c8fc47de
Adam Gao <144167489+agao0224@users.noreply.github.com>
2023-12-17 23:35:48 -0700

    Update streamlitdashboard.py
    updated dashboard

--- streamlitdashboard.py
+++ streamlitdashboard.py
@@ -7,36 +7,31 @@ st.title('Popular Names')
 
 df = pd.read_csv('nbadata.csv')
 
-df['YEAR'] = None  # Creating the 'Year' column initially with None values
+#Filter data with old team names to their new respective team names
+df['TEAM'].replace(to_replace = ['NOH'], value = 'NOP', inplace = True)
+df['TEAM'].replace(to_replace = ['SEA'], value = 'OKC', inplace = True)
+df['TEAM'].replace(to_replace = ['VAN'], value = 'MEM', inplace = True)
+df['TEAM'].replace(to_replace = ['CHA'], value = 'CHH', inplace = True)
+df['TEAM'].replace(to_replace = ['NJN'], value = 'BKN', inplace = True)
+
+#Organize the dataset by years players played in
+df['YEAR'] = None  
 df.loc[:227, 'YEAR'] = 2020
 df.loc[228:413, 'YEAR'] = 2010
 df.loc[414:595, 'YEAR'] = 2000
 
-#selected_name = st.text_input('Enter a name', 'John') #First field is prompt, second field is default
-
-#name_df = df[df['name'] == selected_name]
-
-#if name_df.empty:
-#    st.write('Name not found :(')
-#else:
-#    fig = px.line(name_df, x = 'year', y = 'n', color = 'sex',
-#                  color_discrete_sequence = ['blue','red'])
-#    st.plotly_chart(fig)
-
 selected_year = st.selectbox('Select a year', df['YEAR'].unique())
 
 year_df = df[df['YEAR'] == selected_year]
-#fnames = year_df[year_df['sex'] == 'F'].sort_values(by = 'n', ascending = False).head(5)
-#mnames = year_df[year_df['sex'] == 'M'].sort_values(by = 'n', ascending = False).head(5)
+
+#Get the top 10 players in each decade
 top_players_2020 = df[df['YEAR'] == 2020].nsmallest(10, 'RANK')
 top_players_2010 = df[df['YEAR'] == 2010].nsmallest(10, 'RANK')
 top_players_2000 = df[df['YEAR'] == 2000].nsmallest(10, 'RANK')
 
-merged_df = [top_players_2020, top_players_2010, top_players_2000]
+merged_df = [top_players_2020, top_players_2010, top_players_2000, top_players_2000]
 
-top_10_players = pd.concat(merged_df)
+top_10_players = pd.concat([top_players_2020, top_players_2010], axis = 0)
 
-#top_names = pd.concat([fnames, mnames], axis = 0) 
-
-st.write(f'Top 10 players in {selected_year}:')
+st.write(f'Top 10 in players in {selected_year}:')
 st.dataframe(top_10_players)
