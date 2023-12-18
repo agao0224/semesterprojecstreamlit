809a3304ed9d6db16389c026b61d0a9adac30236
Adam Gao <144167489+agao0224@users.noreply.github.com>
2023-12-17 23:03:07 -0700

    Update streamlitdashboard.py
    updated streamlit

--- streamlitdashboard.py
+++ streamlitdashboard.py
@@ -28,8 +28,15 @@ selected_year = st.selectbox('Select a year', df['YEAR'].unique())
 year_df = df[df['YEAR'] == selected_year]
 #fnames = year_df[year_df['sex'] == 'F'].sort_values(by = 'n', ascending = False).head(5)
 #mnames = year_df[year_df['sex'] == 'M'].sort_values(by = 'n', ascending = False).head(5)
+top_players_2020 = df[df['YEAR'] == 2020].nsmallest(10, 'RANK')
+top_players_2010 = df[df['YEAR'] == 2010].nsmallest(10, 'RANK')
+top_players_2000 = df[df['YEAR'] == 2000].nsmallest(10, 'RANK')
+
+merged_df = [top_players_2020, top_players_2010, top_players_2000]
+
+top_10_players = pd.concat(merged_df)
 
 #top_names = pd.concat([fnames, mnames], axis = 0) 
 
-st.write(f'Top names in {selected_year}:')
-#st.dataframe(top_names)
+st.write(f'Top 10 in {selected_year}:')
+st.dataframe(top_10_players)
