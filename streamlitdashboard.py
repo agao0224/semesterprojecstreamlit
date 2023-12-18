85bc45df98cdeb91c17459deba21951a9dce35c0
Adam Gao <144167489+agao0224@users.noreply.github.com>
2023-12-18 00:48:43 -0700

    Update streamlitdashboard.py

--- streamlitdashboard.py
+++ streamlitdashboard.py
@@ -37,47 +37,36 @@ selected_decade_df = top_10_players[top_10_players['YEAR'] == selected_decade]
 st.write(f'Top 10 players in {selected_decade}:')
 st.dataframe(selected_decade_df)
 
-def univariate_stats(df,gen_charts):
-  #Step 1 generate output dataframe
-  import pandas as pd
-  import seaborn as sns
-  from matplotlib import pyplot as plt
-  import matplotlib.pyplot as plt
-  output_df = pd.DataFrame(columns=['Count','Null' ,'Unique', 'Type', 'Min', 'Max', '25%', '50%', '75%', 'Mean', 'Mode', 'Std', 'Skew', 'Kurt'])
+def univariate_stats(df, col):
+    import pandas as pd
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import streamlit as st
+    output_df = pd.DataFrame(columns=['Count', 'Null', 'Unique', 'Type', 'Min', 'Max', '25%', '50%', '75%', 'Mean', 'Mode', 'Std', 'Skew', 'Kurt'])
 
-  #Step 2 iterate through columns in df
-  for col in df:
-
-  #Step 3 determine if column is numeric
     if pd.api.types.is_numeric_dtype(df[col]):
-    #Step 4a generate stats for numeric col
-      oRow = [df[col].count(),df[col].isna().sum(),df[col].nunique(),df[col].dtype,df[col].min(),df[col].max(),
-      df[col].quantile(0.25),df[col].median(),df[col].quantile(0.75),round(df[col].mean(),2),
-      round(df[col].mode()[0],2),df[col].std(),df[col].skew(),df[col].kurt()]
-      output_df.loc[col] = oRow
-      #Step 5a generate hist plot if boolean is true
-      if gen_charts:
-        str_text = 'Kurtosis is ' + str(df[col].kurt()) +'\n' + 'Skew is ' + str(df[col].skew())
-        plt.text(max(df[col]) + (max(df[col])/10), 54,str_text)
-        sns.histplot(data =df, x = col)
-        plt.show()
+        oRow = [df[col].count(), df[col].isna().sum(), df[col].nunique(), df[col].dtype, df[col].min(), df[col].max(),
+                df[col].quantile(0.25), df[col].median(), df[col].quantile(0.75), round(df[col].mean(), 2),
+                round(df[col].mode()[0], 2), df[col].std(), df[col].skew(), df[col].kurt()]
+        output_df.loc[col] = oRow
+
+        fig, ax = plt.subplots()
+        ax.text(max(df[col]) + (max(df[col]) / 10), 54, f'Kurtosis is {df[col].kurt()}\nSkew is {df[col].skew()}')
+        sns.histplot(data=df, x=col, ax=ax)
+        st.pyplot(fig)
+
     else:
-      #Step 4b generate stats for categorical col
-      oRow = [df[col].count(), df[col].isna().sum(), df[col].nunique(),df[col].dtype,'-','-',
-      '-','-','-','-',
-      df[col].mode()[0],'-','-','-']
+        oRow = [df[col].count(), df[col].isna().sum(), df[col].nunique(), df[col].dtype, '-', '-', '-', '-', '-',
+                '-', df[col].mode()[0], '-', '-']
+        output_df.loc[col] = oRow
 
-      #Step 5b generate count plot if boolean is true
-      if gen_charts:
-        plt.xticks(rotation =45)
-        sns.countplot(data =df, x = col, palette="Greens_d",
-                      order=df[col].value_counts().iloc[:6].index)
-        plt.show()
-    #Step 6 add row to dataframe
-    output_df.loc[col] = oRow
+        fig, ax = plt.subplots()
+        ax.set_xticks(rotation=45)
+        sns.countplot(data=df, x=col, palette="Greens_d", order=df[col].value_counts().iloc[:6].index, ax=ax)
+        st.pyplot(fig)
 
-  #step 7 output df
-  return output_df
+    #step 7 output df
+    return output_df
 
 df_filtered = df.drop(columns=['PLAYER_ID', 'PLAYER', 'TEAM', 'TEAM_ID'])
 
