7f696ad68dadd07a9f8469efbcad94b95fee59e7
Adam Gao <144167489+agao0224@users.noreply.github.com>
2023-12-18 21:34:46 -0700

    Update streamlitdashboard.py
    updating streamlit

--- streamlitdashboard.py
+++ streamlitdashboard.py
@@ -66,3 +66,36 @@ selected_column = st.selectbox('Select a column', df_filtered.columns)
 # Display the distribution for the selected column
 if st.button('Generate Chart'):
     output = univariate_stats(df_filtered, selected_column, True)
+
+
+
+combined_top_players = pd.concat([top_players_2020, top_players_2010, top_players_2000])
+
+# Function to generate plots based on selected statistic
+def generate_plot(statistic):
+    decades = combined_top_players['YEAR'].unique()
+
+    plt.figure(figsize=(10, 6))
+    for decade in decades:
+        filtered_data = combined_top_players[combined_top_players['YEAR'] == decade]
+
+        average_stat = filtered_data.groupby('YEAR')[statistic].mean()
+
+        plt.plot(average_stat.index, average_stat.values, marker='o', label=f'{decade}s')
+
+    plt.title(f'Average {statistic} of Top 10 Players at the Beginning of Each Decade')
+    plt.ylabel(f'Average {statistic}')
+    plt.xlabel('Year')
+    plt.legend()
+    st.pyplot(plt)
+
+# Streamlit app
+st.title('NBA Player Stats Comparison')
+
+# Dropdown for statistics
+statistic_options = ['PTS', 'AST', 'REB', 'FG_PCT', 'MIN', 'EFF', 'FG3_PCT', 'FG3M', 'GP']
+selected_statistic = st.selectbox('Select a Statistic', statistic_options)
+
+# Generate plot on button click
+if st.button('Generate Plot'):
+    generate_plot(selected_statistic)
